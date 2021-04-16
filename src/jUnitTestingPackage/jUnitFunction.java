16ccf5356b53bc26f0192619d5790eede1fcf65e
Abhishek Goel <dell@DESKTOP-FR9PM7H>
2021-04-16 22:52:05 +0530

    Unit Testing

---
+++ src/jUnitTestingPackage/jUnitFunction.java
@@ -0,0 +1,12 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+	public int addNumbers(int Num1, int Num2) {
+		return Num1+Num2;
+	}
+	
+	public String addStrings(String s1, String s2) {
+		return s1+s2;
+	}
+
+}
